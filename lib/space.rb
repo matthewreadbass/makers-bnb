bb628167dca9553576e59eef8205951cc9355389
Lauren Ashton <l.v.ashton@outlook.com>
2021-10-19 16:51:25 +0100

    Added space with methods for adding to db

---
+++ lib/space.rb
@@ -0,0 +1,24 @@
+require "pg"
+
+class Space
+  def self.all
+    if ENV["ENVIRONMENT"] == "test"
+      db = PG.connect(dbname: "makersbnb_test")
+    else
+      db = PG.connect(dbname: "makersbnb")
+    end
+
+    results = db.exec("SELECT * FROM spaces;")
+    results.map { |space| { title: space["title"], description: space["description"], price: space["price"], available_from: space["available_from"], available_to: space["available_to"] } }
+  end
+
+  def self.add(title, description, price, available_from, available_to)
+    if ENV["ENVIRONMENT"] == "test"
+      db = PG.connect(dbname: "makersbnb_test")
+    else
+      db = PG.connect(dbname: "makersbnb")
+    end
+
+    db.exec("INSERT INTO spaces (title, description, price, available_from, available_to) VALUES('#{title}', '#{description}', '#{price}', '#{available_from}', '#{available_to}');")
+  end
+end
---
+++ spec/space_spec.rb
@@ -0,0 +1,23 @@
+require "space"
+
+describe Space do
+  describe "#all" do
+    it "returns all spaces" do
+      Space.add("Las Vegas", "Renting this place", "100", "2021-10-19", "2021-10-26")
+
+      spaces = Space.all
+
+      expect(spaces).to include({ title: "Las Vegas", description: "Renting this place", price: "100", available_from: "2021-10-19", available_to: "2021-10-26" })
+    end
+  end
+
+  describe "#add" do
+    it "adds new space to database" do
+      Space.add("Las Vegas", "Renting this place", "100", "2021-10-19", "2021-10-26")
+
+      spaces = Space.all
+
+      expect(spaces).to include({ title: "Las Vegas", description: "Renting this place", price: "100", available_from: "2021-10-19", available_to: "2021-10-26" })
+    end
+  end
+end
