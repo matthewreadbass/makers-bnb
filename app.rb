3459387d4ecaf0195ba54348f5168a39ce0a9a95
Lauren Ashton <l.v.ashton@outlook.com>
2021-10-20 11:20:27 +0100

    Refactored sign up method

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
-require "pg"
+require "./lib/user"
 
 class Makersbnb < Sinatra::Base
   configure :development do
@@ -17,18 +17,17 @@ class Makersbnb < Sinatra::Base
   post "/log_in" do
     "Welcome"
   end
-  
+
   post "/sign_up" do
     if params[:password] != params[:password2]
       # erb(:passwords_dont_match)
       "Error, passwords do not match"
     else
-    @name = params[:name]
-    @email = params[:email]
-    @password = params[:password]
-    connection = PG.connect(dbname: 'makersbnb') 
-    connection.exec("INSERT INTO users (first_name, email, password) VALUES ('#{@name}','#{@email}','#{@password}');")
-    "You have created an account"
+      @name = params[:name]
+      @email = params[:email]
+      @password = params[:password]
+      User.create(@name, @email, @password)
+      "You have created an account"
     end
   end
 end
