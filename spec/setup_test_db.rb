0688be4b66a0c0b674499d3dd241f1ab96e48a4d
parsa <parsamesg@gmail.com>
2021-10-20 11:07:56 +0100

    setup test database function for resetting after every run

---
+++ spec/setup_test_db.rb
@@ -0,0 +1,10 @@
+require "pg"
+
+def setup_test_db
+  p "Setting up test db..."
+
+  db = PG.connect(dbname: "makersbnb_test")
+
+  db.exec("TRUNCATE TABLE users RESTART IDENTITY;")
+  db.exec("TRUNCATE TABLE spaces RESTART IDENTITY;")
+end
