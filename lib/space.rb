e13c4072d195b8de88f7f1aef7796afbb94379bf
Lauren Ashton <l.v.ashton@outlook.com>
2021-10-21 20:08:37 +0100

    Cleaned up commented out code

--- lib/space.rb
+++ lib/space.rb
@@ -55,14 +55,3 @@ class Space
     Space.new(id: result[0]["id"], title: result[0]["title"], description: result[0]["description"], price: result[0]["price"], available_from: result[0]["available_from"], available_to: result[0]["available_to"])
   end
 end
-
-#   def self.find(id:)
-#     if ENV["ENVIRONMENT"] == "test"
-#       connection = PG.connect(dbname: "bookmark_manager_test")
-#     else
-#       connection = PG.connect(dbname: "bookmark_manager")
-#     end
-#     result = connection.exec_params("SELECT * FROM bookmarks WHERE id = $1;", [id])
-#     Bookmark.new(id: result[0]["id"], title: result[0]["title"], url: result[0]["url"])
-#   end
-# end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -12,8 +12,6 @@ describe Space do
 
       expect(spaces[0].title).to eq "Las Vegas"
       expect(spaces[0].description).to eq "Renting this place"
-
-      # expect(spaces).to include({ title: "Las Vegas", description: "Renting this place", price: "100", available_from: "2021-10-19", available_to: "2021-10-26" })
     end
   end
 
@@ -30,18 +28,6 @@ describe Space do
       expect(space.id).to eq space.id
     end
   end
-
-  # describe "#find" do
-  #   it "retreives the id of a space from the database" do
-  #     space = Space.add("Cozy Cottage", "Small cottage in the Cotswolds", "100", "2021-10-25", "2021-11-25")
-  #     connection = PG.connect(dbname: "makersbnb_test")
-  #     id = connection.exec_params("SELECT id FROM spaces WHERE title = 'Cozy Cottage';")
-
-  #     result = Space.find(id[0]["id"])
-
-  #     expect(result.to_a).to eq([{ "available" => "t", "available_from" => "2021-10-25", "available_to" => "2021-11-25", "description" => "Small cottage in the Cotswolds", "id" => "1", "price" => "100", "title" => "Cozy Cottage" }])
-  #   end
-  # end
 
   describe ".find" do
     it "returns the requested bookmark object" do
